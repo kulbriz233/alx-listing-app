36464ea04ca4d923a354bea85b54e1a8e997a097
Augustine Ninyung <ninyungaugustine@gmail.com>
2025-06-05 17:10:32 +0000

    Trials

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,29 @@ export const API_BASE_URL = 'https://api.example.com'
 export const PLACWEHOLDER_IMAGE = '/assets/placeholder.jpg'
 export const API_CBASE_URL = 'https://api.example.com'
 export const PLACEHOLDER_IMAGE = '/assets/placeholder.jpg'
+
+
+There is a new line here abi you wan mk we do am function
+
+index.import { connect } from 'react-redux'
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export class index extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(index)
